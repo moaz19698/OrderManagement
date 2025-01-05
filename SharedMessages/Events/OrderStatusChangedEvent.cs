0401947c3c4a570a30312ad7441ceb38d0e81e0b
hp <hp@DESKTOP-GRJIGAN>
2025-01-05 19:56:24 +0200

    add SharedMessages when you need to make common layer

---
+++ SharedMessages/Events/OrderStatusChangedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedMessages.Events
+{
+    public class OrderStatusChangedEvent
+    {
+        public Guid OrderId { get; set; }
+        public string OldStatus { get; set; }
+        public string NewStatus { get; set; }
+        public DateTime ChangedAt { get; set; }
+    }
+}
